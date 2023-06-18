f9e7bb91cd55ce1aa4780afe618fb9d15ac1fab9
Enes Gülcü <enes.gulcu@hotmail.com>
2023-06-18 22:01:24 +0300

    test

--- functions/mainSearch/index.tsx
+++ functions/mainSearch/index.tsx
@@ -6,51 +6,42 @@ const MainSearch = (inputData:any) => {
     const filteredData:any = []
     const filteredKeyDada:any = []
     const statistics:any = []
-
-    if(inputData.length > 2){
-        data.map((item:any, itemIndex:number) => {
-            item.subcategories.map((subcatagory:any, subcatagoryIndex:number) => {
-                
-                const dbSentence = subcatagory.searchTerm
-                const dbWordArray = dbSentence.split(' ') // veri tabanındaki cümleleri kelime kelime ayırdık ve arraye attık
-                const inputword = inputData.toLowerCase().split(' ') // inputtaki cümleleri kelime kelime ayırdık ve arraye attık
     
-                dbWordArray.map((databaseWord:any, wordIndex:number) => {
-                    const mydata = LevenshteinDistance(inputData.toLowerCase(), databaseWord.toLowerCase())
-                    if(Number(mydata) > 50 && subcatagory.searchTerm.length > 2){
-                        statistics.push(
-                            
-                                {
+    
+    data.map((item:any, itemIndex:number) => {
+        
+        item.subcategories.map((subcatagory:any, subcatagoryIndex:number) => {
 
-                                    'inputData ': inputData.toLowerCase(),
-                                    'databaseWord ': databaseWord.toLowerCase(),
-                                    'mydata': mydata,
-                                    'subcatagory.searchTerm': subcatagory.searchTerm,
-                                    'databaseWord': dbWordArray,
-                                    "wordIndex": wordIndex,
-                                }
-                            
-                        )
+            //subcatagory.searchAvarage -> veri tabanındaki cümlelerin aranma ortalaması
+            //subcatagory.searchTerm -> veri tabanındaki cümleler
+
+            const dbSentence = subcatagory.searchTerm
+            const dbWordArray = dbSentence.split(' ') // veri tabanındaki cümleleri kelime kelime ayırdık ve arraye attık
+            const inputword = inputData.toLowerCase().split(' ') // inputtaki cümleleri kelime kelime ayırdık ve arraye attık
+
+            dbWordArray.map((databaseWord:any, wordIndex:number) => {
+                const mydata = LevenshteinDistance(inputData.toLowerCase(), databaseWord.toLowerCase())
+
+                if((mydata && Number(mydata) > 40) || (inputData.lenght > 2 && databaseWord.toLowerCase().startsWith(inputword[0]) &&filteredData && !filteredData.includes(subcatagory.searchTerm))){
+                    
+                    if(!filteredData.includes(subcatagory.searchTerm)){
+                        filteredData.push(subcatagory.searchTerm)
                     }
-                    if(Number(mydata) > 50 && subcatagory.searchTerm.length > 2){
-                        
-                        if(!filteredData.includes(subcatagory.searchTerm)){
-                            filteredData.push(subcatagory.searchTerm)
-                        }
-                    }
+                }
+                
+               
+                if(databaseWord.toLowerCase().startsWith(inputword[0])){
                    
-                    if(databaseWord.toLowerCase().startsWith(inputword[0]) && inputData.length > 2){
-                       
-                        if(!filteredData.includes(subcatagory.searchTerm)){
-                            filteredData.push(subcatagory.searchTerm)
-                        }
+                    if(!filteredData.includes(subcatagory.searchTerm)){
+                        filteredData.push(subcatagory.searchTerm)
                     }
-                })    
-            })
+                }
+            })    
         })
-        console.log(statistics)
-        return filteredData;
-    }
+    })
+    console.log(filteredData)
+    //return filteredData;
+    
 
     
     
