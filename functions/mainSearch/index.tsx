b77ce5d66740629e62a806b3f124629d462ad746
Enes Gülcü <enes.gulcu@hotmail.com>
2023-06-18 12:39:08 +0300

    test

--- functions/mainSearch/index.tsx
+++ functions/mainSearch/index.tsx
@@ -5,37 +5,52 @@ const MainSearch = (inputData:any) => {
     const data = Data;
     const filteredData:any = []
     const filteredKeyDada:any = []
-    data.map((item:any, itemIndex:number) => {
-        item.subcategories.map((subcatagory:any, subcatagoryIndex:number) => {
-            
-            const word = subcatagory.searchTerm
-            const wordArray = word.split(' ')
-            const inputword = inputData.toLowerCase().split(' ')
-            wordArray.map((a:any, wordIndex:number) => {
-                const mydata = LevenshteinDistance(inputData.toLowerCase(), a.toLowerCase())
+    const statistics:any = []
+
+    if(inputData.length > 2){
+        data.map((item:any, itemIndex:number) => {
+            item.subcategories.map((subcatagory:any, subcatagoryIndex:number) => {
                 
-                if(Number(mydata) > 50 && subcatagory.searchTerm.length > 2){
-                    
-                    if(!filteredData.includes(subcatagory.searchTerm)){
-                        filteredData.push(subcatagory.searchTerm)
+                const dbSentence = subcatagory.searchTerm
+                const dbWordArray = dbSentence.split(' ') // veri tabanındaki cümleleri kelime kelime ayırdık ve arraye attık
+                const inputword = inputData.toLowerCase().split(' ') // inputtaki cümleleri kelime kelime ayırdık ve arraye attık
+    
+                dbWordArray.map((databaseWord:any, wordIndex:number) => {
+                    const mydata = LevenshteinDistance(inputData.toLowerCase(), databaseWord.toLowerCase())
+                    if(Number(mydata) > 50 && subcatagory.searchTerm.length > 2){
+                        statistics.push(
+                            
+                                {
+
+                                    'inputData ': inputData.toLowerCase(),
+                                    'databaseWord ': databaseWord.toLowerCase(),
+                                    'mydata': mydata,
+                                    'subcatagory.searchTerm': subcatagory.searchTerm,
+                                    'databaseWord': dbWordArray,
+                                    "wordIndex": wordIndex,
+                                }
+                            
+                        )
                     }
-                }
-               
-                if(a.toLowerCase().startsWith(inputword[0]) && inputData.length > 2){
+                    if(Number(mydata) > 50 && subcatagory.searchTerm.length > 2){
+                        
+                        if(!filteredData.includes(subcatagory.searchTerm)){
+                            filteredData.push(subcatagory.searchTerm)
+                        }
+                    }
                    
-                    if(!filteredData.includes(subcatagory.searchTerm)){
-                        filteredData.push(subcatagory.searchTerm)
+                    if(databaseWord.toLowerCase().startsWith(inputword[0]) && inputData.length > 2){
+                       
+                        if(!filteredData.includes(subcatagory.searchTerm)){
+                            filteredData.push(subcatagory.searchTerm)
+                        }
                     }
-                }
-            })  
-
-
-            
-           
+                })    
+            })
         })
-    })
-
+        console.log(statistics)
         return filteredData;
+    }
 
     
     
